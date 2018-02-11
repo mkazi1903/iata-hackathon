4269bdb905afb23c5afe7a9283765fafea59a048
Mohammad <mkazi-github@gmail.com>
2018-02-11 13:55:55 +0530

    setup of a simple rest controller

---
+++ README.md
@@ -0,0 +1,7 @@
+###1. Technologies
+
+Spring 4.3.1.RELEASE
+Maven 3.3.3
+###2. To Run this project locally
+
+$ mvn tomcat7:run
---
+++ src/main/java/com/linkedin/iata/config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.linkedin.iata.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.linkedin.iata")
+public class AppConfig {
+
+}
